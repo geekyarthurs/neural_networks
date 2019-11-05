32f80331e1591d878ae8a66a1e7fe6e03471f9aa
Mahesh C. Regmi <geekyarthurs@gmail.com>
2019-11-05 15:37:43 +0545

    added matplotlib

--- regression_numpy.py
+++ regression_numpy.py
@@ -1,17 +1,19 @@
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 
 class RegressionModel:
-    def __init__(self, x_train, y_train):
+    def __init__(self, x, y):
 
-        assert(len(x_train) == len(y_train))
+        assert(len(x) == len(y))
 
-        x = x_train
+        self.x = x
         x_squared = np.square(x)
         x_sum = np.sum(x)
         x_squared_sum = np.sum(x_squared)
 
-        y = y_train
+        self.y = y
         y_sum = np.sum(y)
 
         xy = x * y
@@ -21,7 +23,22 @@ class RegressionModel:
             (len(x) * x_squared_sum - x_sum ** 2)
 
         self.intercept = (y_sum - self.slope * x_sum) / len(x)
-    
-    def predict(self , x):
+
+    def predict(self, x):
         return self.slope * x + self.intercept
+
+    def plot(self):
+        print("OK")
+        plt.plot(self.x, self.y)
+        plt.show()
+
+
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 4, 6, 8])
+
+
+model = RegressionModel(a,b)
+
+model.plot()
 
